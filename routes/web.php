4cb9e3f2db4df047a120ab854f18debb60c3ce23
Rosca Filadelfia <rosca.filadelfia@elev.cihcahul.md>
2024-02-06 11:12:15 +0200

    Relatii, rute, migrari

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\PhoneController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\PhoneController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +20,13 @@ Route::redirect('/','/users');
 Route::get('/users',[UserController::class, 'index'])->name('users.index');
 Route::get('/users/create',[UserController::class,'create'])->name('users.create');
 Route::post('/users',[UserController::class,'store'])->name('users.store');
+
+Route::controller(PhoneController::class)->group(function(){
+    Route::get('phones','index');
+    Route::get('phones/create','create');
+    Route::post('phones','store');
+    Route::get('phones/{phone}','show');
+    Route::get('phones/{phone}/edit','edit');
+    Route::put('phones/{phone}/update','update');
+    Route::delete('phones/{phone}','destroy');
+});
